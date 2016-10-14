9fd4529cfa26708dfc97515ef0a5218503b1b244
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2016-10-14 13:50:57 +0800

    Added public folder

--- server/main.js
+++ server/main.js
@@ -6,7 +6,11 @@ var app = express();
 
 // CREATE a config for PORT
 const PORT = 3000;
+const CLIENT_FOLDER = __dirname + "/../client";
 
+console.log("Serving Client folder at ", CLIENT_FOLDER);
+
+app.use(express.static(CLIENT_FOLDER));
 
 // Start server on PORT
 app.listen(PORT, function () {
