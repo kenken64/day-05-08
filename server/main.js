ce0cce3f47741c56518b28cbfca9265456e3998d
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2016-10-14 15:52:55 +0800

    Added exercise for day 05-04

--- server/main.js
+++ server/main.js
@@ -12,54 +12,6 @@ console.log("Serving Client folder at ", CLIENT_FOLDER);
 
 app.use(express.static(CLIENT_FOLDER));
 
-app.get("/showReq", function (req, res) {
-    var reqProperties = {
-        hostName: req.hostName,
-        baseUrl: req.baseUrl,
-        cookies: req.cookies,
-        ip: req.ip,
-        secure: req.secure
-    };
-    // Solution 4a
-    console.log(reqProperties);
-
-    // Solution 4b - you can also use .send and pass a string
-    res.json(reqProperties);
-});
-
-
-app.get("/sample", function (req, res) {
-    res.json({
-        get: true
-    })
-});
-app.put("/sample", function (req, res) {
-    res.json({
-        put: true
-    })
-});
-app.post("/sample", function (req, res) {
-    res.json({
-        post: true
-    })
-});
-app.use("/page-that-throw-error", function (req, res) {
-    console.log("/page accessed")
-    throw new Error("SIMPLE CUSTOM ERROR");
-});
-
-
-
-// Solution 5a - 404 Handler
-app.use(function (req, res) {
-    res.redirect("/page-not-found.html");
-});
-
-// Solution 5b - 501 or error Handler
-app.use(function (err, req, res, next) {
-    res.redirect("/501.html");
-});
-
 // Start server on PORT
 app.listen(PORT, function () {
     console.log("Running server on http://localhost:%s", PORT);
