0579db85e2d245567683325629e60e7497713e54
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2016-10-14 16:52:16 +0800

    Added bower

--- server/main.js
+++ server/main.js
@@ -13,14 +13,9 @@ console.log("Serving Client folder at ", CLIENT_FOLDER);
 app.use(express.static(CLIENT_FOLDER));
 
 
-app.use(function (req, res) {
-    res.redirect("/page-not-found.html");
-});
-
 app.use(function (err, req, res, next) {
     res.redirect("/501.html");
 });
-
 
 
 // Start server on PORT
